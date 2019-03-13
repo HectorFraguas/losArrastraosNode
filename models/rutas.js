8f92ed3b8c0a2863f0f31ef949c8022e9deab951
Fraguas <fraguas14@gmail.com>
2019-03-13 14:46:21 +0100

    parte de comentarios terminada

--- models/rutas.js
+++ models/rutas.js
@@ -9,7 +9,7 @@ let getAll = (done) => {
 }
 
 let getRuta = (id, done) => {
-    db.get().query('select * from rutas as r, usuarios as u WHERE r.id = ? AND r.fk_usuarios = u.id', [id], (err, result) => {
+    db.get().query('SELECT r.id AS idRuta, r.provincia, r.salida, r.llegada, r.descripcion, r.tipoRuta, r.latitud, r.longitud, r.fk_usuarios, u.id, u.nombre, u.usuario, u.email, u.imagen, u.token from rutas as r, usuarios as u WHERE r.id = ? AND r.fk_usuarios = u.id', [id], (err, result) => {
         if(err) return done(err)
         done(null, result) 
     })
@@ -36,6 +36,20 @@ let deletedRuta = (id, done) => {
     })
 }
 
+let agregarComentarioRuta = ({token, fk_ruta, comentario}, done) => {
+    db.get().query('insert INTO comentarios (comentario, fk_ruta, fk_usuario) VALUES (?, ?, (SELECT id FROM usuarios WHERE token = ?))', [comentario, fk_ruta, token], (err, result) => {
+        if(err) return done(err)
+        done(null, result)
+    })
+}
+
+let recuperarComentarios = (id, done) => {
+    db.get().query('SELECT c.comentario, u.usuario as usuarioComentario, u.token as tokenUsuario FROM comentarios AS c JOIN usuarios AS u WHERE c.fk_ruta = ? AND c.fk_usuario = u.id', [id], (err, result) => {
+        if(err) return done(err)
+        done(null, result)
+    })
+}
+
 
 module.exports = {
     getAll: getAll,
@@ -43,4 +57,6 @@ module.exports = {
     deleteRuta: deletedRuta,
     filtroRutas: filtroRutas,
     getRuta: getRuta,
+    agregarComentarioRuta: agregarComentarioRuta,
+    recuperarComentarios: recuperarComentarios
 }
--- routes/rutas.js
+++ routes/rutas.js
@@ -7,15 +7,18 @@ const rutasModel = require('../models/rutas')
 // http://localhost:3000/rutas/all
 router.get('/all', (req,res) => {
     rutasModel.getAll((err, rows) => {
-        console.log(rows)
         res.json(rows)
     })
 })
 
 // http://localhost:3000/ruta/:id
 router.get('/:id', (req, res) => {
-    rutasModel.getRuta(req.params.id, (err,rows) => {
-        res.json(rows[0])
+    console.log(req.params)
+    rutasModel.getRuta(req.params.id, (err,rowsr) => {
+        rutasModel.recuperarComentarios(req.params.id, (err, rowsc) => {
+            rowsr[0].comentarios = rowsc
+            res.json(rowsr[0])
+        })
     })
 })
 
@@ -30,7 +33,6 @@ router.post('/filtro', (req,res) => {
 
 // http://localhost:3000/rutas/new
 router.post('/new', (req, res) => {
-    console.log(req.body)
     rutasModel.createRuta(req.body, (err, result) => {
         if(err) return console.log(err.message)
         res.json(result)
@@ -40,6 +42,14 @@ router.post('/new', (req, res) => {
 // http://localhost:3000/rutas/delete/id
 router.get('/delete/:id', (req, res) => {
     rutasModel.deleteRuta(req.params.id, (err, rows) => {
+        res.json(rows)
+    })
+})
+
+// http://localhost:3000/rutas/agregarcomentario
+router.post('/agregarcomentario', (req, res) => {
+    rutasModel.agregarComentarioRuta(req.body, (err, rows) => {
+        if(err) return console.log(err.message)
         res.json(rows)
     })
 })
